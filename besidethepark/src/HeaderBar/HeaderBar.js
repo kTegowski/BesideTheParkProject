204acd245183e448207a9d7ca0cd7db30d227b87
kTegowski <104396432+kTegowski@users.noreply.github.com>
2023-02-24 23:27:26 +0100

    Write code of HeaderBar, add font and colors

---
+++ besidethepark/src/HeaderBar/HeaderBar.js
@@ -0,0 +1,10 @@
+import './HeaderBar.css';
+function HeaderBar() {
+    return (
+      <div className="HeaderBar">
+        <h1>LOREM IPSUM</h1>
+      </div>
+    );
+  }
+  
+  export default HeaderBar;
