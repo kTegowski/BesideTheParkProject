520f432fec032e7b485e73df29e4d623b5209a8b
kTegowski <104396432+kTegowski@users.noreply.github.com>
2023-02-24 23:23:34 +0100

    Add TitleBar.js file, creat first element of site

---
+++ besidethepark/src/TitleBar/TitleBar.js
@@ -0,0 +1,17 @@
+import './TitleBar.css';
+import logo from './ImageView//logo.png';
+function TitleBar() {
+    return (
+      <div id="TitleBar">
+        <div className="description">
+             Episode of the  <b> 4th </b>  season of the series 
+        </div>
+        <h1 className="seriesName">
+            Rick and Morty
+        </h1>
+        <img src={logo} className="logo" alt = "logo" />
+      </div>
+    );
+  }
+  
+  export default TitleBar;
