2b7f099541468aff1bfd14727f4e5f76ba162ab5
kTegowski <104396432+kTegowski@users.noreply.github.com>
2023-02-27 20:05:11 +0100

    Add tests to chcek epsiode and title

--- besidethepark/src/App.test.js
+++ besidethepark/src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders episode firs from API', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/S04E01/i);
   expect(linkElement).toBeInTheDocument();
+  const linkElement1 = screen.getByText(/S04E10/i);
+  expect(linkElement1).toBeInTheDocument();
 });
+test('title in the site',() =>{
+  render(<App/>);
+  const linkElement2 = screen.getByText(/Episode of the 4th season of the series/i)
+})
